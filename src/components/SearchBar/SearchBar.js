1d720156d7879ab409ed59d6d7138db2f32a347b
ricacostt <ricacostt@gmail.com>
2023-02-13 10:07:25 +0100

    resize search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,8 +11,8 @@ function SearchBar(props) {
   return (
  <div className="form-control">
       <div className="input-group justify-center mt-10 mb-10">
-        <input type="text" placeholder="Search by title…" className="input input-bordered h-20 w-96 text-2xl" onChange={handleUserInput} value={userInput} />
-        <button onClick={handleClick} className="btn btn-square  h-20 w-20">
+        <input type="text" placeholder="Search by title…" className="input input-bordered h-16 w-96 text-2xl" onChange={handleUserInput} value={userInput} />
+        <button onClick={handleClick} className="btn btn-square  h-16 w-16">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
         </button>
       </div>
